181f74aa8dbff34f980f9e8547a77e5ae191118f
philschmid <schmidphilipp1995@gmail.com>
2023-07-30 00:15:27 +0200

    0.1.0

--- easyllm/__init__.py
+++ easyllm/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present philschmid <schmidphilipp1995@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 import os
 import sys
